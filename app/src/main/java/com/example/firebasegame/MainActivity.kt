b83ad91fdf7773d592c52e5c16401d197728894b
Thomas Ferris <A01211467@ad.bcit.ca>
2020-03-01 19:08:32 -0800

    connection to firebase and user screen

--- app/src/main/java/com/example/firebasegame/MainActivity.kt
+++ app/src/main/java/com/example/firebasegame/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.firebasegame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }

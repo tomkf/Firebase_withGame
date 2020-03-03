6d0abcccc6d6393dec786c3c543368eb5539f54f
Thomas Ferris <A01211467@ad.bcit.ca>
2020-03-02 19:10:19 -0800

    game activity

--- app/src/main/java/com/example/firebasegame/AuthActivity.kt
+++ app/src/main/java/com/example/firebasegame/AuthActivity.kt
@@ -4,20 +4,52 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_auth.*
 
 class AuthActivity: AppCompatActivity() {
+    // lateinit is 'lazy'
+    private lateinit var auth: FirebaseAuth
     private lateinit var authMode: AuthMode
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_auth)
 
         authMode = intent?.extras?.getParcelable(AuthModeExtraName)!!
+        auth = FirebaseAuth.getInstance()
 
         when(authMode) {
             is AuthMode.Login -> { auth_button.text = getString(R.string.login)}
             is AuthMode.Register -> { auth_button.text = getString(R.string.register) }
+        }
+
+        auth_button.setOnClickListener {
+            when(authMode) {
+                is AuthMode.Login -> {
+                    auth.signInWithEmailAndPassword(email_input.text.toString(), password_input.text.toString())
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                val user = auth.currentUser
+                                startActivity(Intent(this, StartGameActivity::class.java))
+                            }
+
+                            // Handle error here
+                        }
+                }
+
+                is AuthMode.Register -> {
+                    auth.createUserWithEmailAndPassword(email_input.text.toString(), password_input.text.toString())
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                val user = auth.currentUser
+                                startActivity(Intent(this, StartGameActivity::class.java))
+                            }
+
+                            // Handle error here
+                        }
+                }
+            }
         }
     }
 
